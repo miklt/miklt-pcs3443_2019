925c6dea9af2cfd75a07b04121069c99eabda9d2
Eric Imai <ericimai@hotmail.com>
2019-05-08 09:33:23 -0300

    usuarioSchema completo

--- usuarioSchema.py
+++ usuarioSchema.py
@@ -2,4 +2,7 @@ from marshmallow import Schema, fields
 from lista.models.usuario import Usuario
 
 class ItemSchema(Schema):
-  nome 
+  usuario_id = fields.Integer()
+  nome = fields.Str()
+  email = fields.Str()
+  senha = fields.Str()
