dcff9990dc372688948d873bd31e2a09ce1d20ff
Eric Imai <ericimai@hotmail.com>
2019-05-08 10:19:16 -0300

    modificacoes requirements.txt e __init__ funcional

--- project/__init__.py
+++ project/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_mail import Mail
+#from flask_mail import Mail
 from project.config import Config
 
 
@@ -11,7 +11,7 @@ bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-mail = Mail()
+#mail = Mail()
 
 
 def create_app(config_class=Config):
@@ -21,15 +21,15 @@ def create_app(config_class=Config):
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-    mail.init_app(app)
+    #mail.init_app(app)
 
-    from flaskblog.users.routes import users
-    from flaskblog.posts.routes import posts
-    from flaskblog.main.routes import main
-    from flaskblog.errors.handlers import errors
-    app.register_blueprint(users)
-    app.register_blueprint(posts)
-    app.register_blueprint(main)
-    app.register_blueprint(errors)
+    #from flaskblog.users.routes import users
+    #from flaskblog.posts.routes import posts
+    #from flaskblog.main.routes import main
+    #from flaskblog.errors.handlers import errors
+    #app.register_blueprint(users)
+    #app.register_blueprint(posts)
+    #app.register_blueprint(main)
+    #app.register_blueprint(errors)
 
     return app
--- project/forms/usuarioSchema.py
+++ project/forms/usuarioSchema.py
@@ -1,8 +1,10 @@
 from marshmallow import Schema, fields
-from lista.models.usuario import Usuario
+from project.models.usuarios import Usuario
 
 class ItemSchema(Schema):
   usuario_id = fields.Integer()
   nome = fields.Str()
   email = fields.Str()
   senha = fields.Str()
+  class Meta:
+    model = Usuario
