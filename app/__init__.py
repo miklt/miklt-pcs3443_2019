e517f74680cf30f588c994f29f3a47d21dca2b8b
bruno <brunob.inacio@outlook.com.br>
2019-05-09 11:04:56 -0300

    login database

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,15 @@
 from flask import Flask
 from flask import render_template
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-
-# Configurations
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-
-from . import test, login
-
-
-# Import DB (to do)
-# Define the WSGI application object
-# Define the database object which is imported
-# by modules and controllers
-#db = SQLAlchemy(app)
-
-# Sample HTTP error handling
-#@app.errorhandler(404)
-#def not_found(error):
-#    return render_template('404.html'), 404
-
-# Import a module / component using its blueprint handler variable (mod_auth)
-from app.login.controllers import login as login
-
-# Register blueprint(s)
+# Blueprints
+from app.login.controllers import login
 app.register_blueprint(login)
-# app.register_blueprint(xyz_module)
-# ..
 
-# Build the database:
-# This will create the database file using SQLAlchemy
-#db.create_all()
+# DB
+db.create_all()
--- app/login/controllers.py
+++ app/login/controllers.py
@@ -1,8 +1,18 @@
 from flask import Blueprint
+from app import db
+from app.login.models import Login
 
 login = Blueprint('login', __name__, url_prefix='/login')
 
-@login.route("/")
-def home():
+@login.route('/', methods=['GET', 'POST'])
+def signin():
+    #email = request.form['email']
+    #password = request.form['password'] 
     return "test"
-    
+
+@login.route("/addTest/")
+def test():
+    u = Login('teste1', 'e@a', 'abcd', 'aluno')
+    db.session.add(u)
+    db.session.commit()
+    return "oi"
