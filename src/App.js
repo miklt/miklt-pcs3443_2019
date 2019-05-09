6b10d6a2be2ef4ce10857b3b3451bdf03de14ef1
Aluno <aluno@labprog.local>
2019-05-09 09:52:03 -0300

    mudanças

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-/**/
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+
+class App extends React.Component {
+
+    render () {
+
+        return (
+            <div class="header">
+                <div class="linha">
+                    <header>
+                        <div class="coluna col 4">
+                            <h1 class="logo">João Cortez</h1>
+                        </div>
+                        <div class="coluna col8">
+                            <nav>
+                                <ul class="menu inline sem-marcador">
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="clientes.html">Clientes</a></li>
+                                    <li><a href="servicos.html">Serviços</a></li>
+                                    <li><a href="sobre.html">Sobre</a></li>
+                                    <li><a href="contato.html">Contato</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </header>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
