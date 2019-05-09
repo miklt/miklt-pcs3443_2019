5000c0741a86f2c5a5834cbc12f5f31295c2839e
Gusgreco <gustavo.greco.85@gmail.com>
2019-05-09 09:36:01 -0300

    Commit 0

--- app.py
+++ app.py
@@ -1,39 +1,6 @@
-# -*- coding: utf-8 -*-
 from flask import Flask
-from flask_cors import CORS
-from flask_restful import Api
-from lista.resources.item_resource import ItemResource, ItensResource
-from lista.resources.usuario_resource import UsuarioResource,UsuariosResource
-from lista.resources.lista_resource import ListaResource, ListasResource
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['SQLALCHEMY_ECHO'] = True
 
-app.secret_key = 'secreto'
-
-#fim configurações relativas ao sqlalchemy
-api = Api(app)
-CORS(app,resources={r"/*": {"origins": "*"}}) #O uso do cors
-#cria as tabelas do banco de dados, caso elas não estejam criadas
-@app.before_first_request
-def create_tables():
-    print("criar tabelas")
-    db.create_all()
-#fim criaçaõ de tabelas
-
-api.add_resource(ItensResource, '/itens')
-api.add_resource(ItemResource, '/item', '/item/<string:item>')
-
-
-api.add_resource(ListasResource, '/listas')
-api.add_resource(ListaResource, '/lista','/lista/<string:lista>')
-api.add_resource(UsuarioResource,'/usuario','/usuario/<string:nome>')
-api.add_resource(UsuariosResource, '/usuarios')
-
-if __name__ == '__main__':
-    from dao import db
-    db.init_app(app)
-    app.run(port=5000,debug=True)
+@app.route("/")
+def home():
+    return "Hello, Flask!"
