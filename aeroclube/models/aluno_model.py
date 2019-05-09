88543cbd886390731b57258c5adffa97a1134539
Gusgreco <gustavo.greco.85@gmail.com>
2019-05-09 10:15:06 -0300

    dao

---
+++ aeroclube/models/aluno_model.py
@@ -0,0 +1,4 @@
+from dao import db,Base
+
+class Aluno(Base):
+    __tablename__ = 'aluno'
--- app.py
+++ app.py
@@ -4,3 +4,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+
+def create_tables():
+    print("criar tabelas")
+    db.create_all()
+#fim criaçaõ de tabelas
+
+if __name__ == '__main__':
+    from dao import db
+    db.init_app(app)
+    app.run(port=5000,debug=True)
