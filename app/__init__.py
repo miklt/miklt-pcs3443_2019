74e99a4747302c6c83dd316a8a1e8a7e6352d2e5
Matheus Vieira <matheus.sousav@gmail.com>
2019-05-17 19:06:31 -0300

    Initial commit
    This repository has the purpose to separate the front-end from the
    back-end of the Stop Down project. If you wish to see the
    changelog of the project before this point, please refer to
    <https://github.com/matthews34/stop_down>.

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+"""
+Este é um arquivo __init__.py, ele serve para que o python identifique que esta pasta é um módulo
+Descrição do arquivo: aqui é criado a blueprint, para a modularização do projeto, a API com
+informações sobre a documentação (título e versão, é possível adicionar descrição também) e são
+adicionados à API os namespaces
+"""
+
+from flask_restplus import Api
+from flask import Blueprint
+
+# importar namespace do controller
+from .main.controller.instrutor_controller import api as instrutor_ns
+from .main.controller.funcionario_controller import api as funcionario_ns
+from .main.controller.piloto_controller import api as piloto_ns
+from .main.controller.voo_controller import api as voo_ns
+from .main.controller.aluno_controller import api as aluno_ns
+from .main.controller.aula_supervisionada_controller import api as aula_supervisionada_ns
+
+blueprint = Blueprint('api', __name__)
+
+api = Api(blueprint,
+          title='STOP DOWN',
+          version='1.0'
+          )
+
+# adicionar namespaces na API, para setar as rotas e a documentação
+api.add_namespace(instrutor_ns, path='/instrutor')
+api.add_namespace(funcionario_ns, path='/funcionario')
+api.add_namespace(piloto_ns, path='/piloto')
+api.add_namespace(voo_ns, path='/voo')
+api.add_namespace(aluno_ns, path='/aluno')
+api.add_namespace(aula_supervisionada_ns, path='/aula_supervisionada')
---
+++ app/main/__init__.py
@@ -0,0 +1,29 @@
+"""
+Este é um arquivo __init__.py, ele serve para que o python identifique que esta pasta é um módulo
+Descrição: aqui é que é definido o objeto db e a função create_app() que seta inicializa o app e
+seta o banco de dados
+Imports deste arquivo:
+    SQLAlchemy: método responsável por criar e configurar a conexão do banco de dados com o Flask
+"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+DATABASE_URL = getenv('DATABASE_URL')
+
+db = SQLAlchemy()
+
+def create_app():
+    # criação do app
+    app = Flask(__name__)
+    CORS(app, origins='*')
+    # configuração da conexão com o banco de dados
+    # a URI segue o seguinte formato:
+    # driver://username:password@host[:port]/database
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+    db.init_app(app)
+    return app
