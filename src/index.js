d76d79f639a5b7312b15ff0396fce2e0c234006f
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2019-05-20 14:19:45 -0300

    Add Aluno signup page
    Add Aluno signup page and adapt index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, BrowserRouter as Router} from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import Cadastro_instrutor from './cadastro/Instrutor';
+import Cadastro_aluno from './cadastro/Aluno';
 import * as serviceWorker from './serviceWorker';
 import Home from './Home';
 
 const routing = (
-  <Router>
-    <div>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/cadastro/instrutor" component={Cadastro_instrutor}/>
-    </div>
-  </Router>
-)
+	<Router>
+		<div>
+			<Route exact path="/" component={Home} />
+			<Route exact path="/cadastro/instrutor" component={Cadastro_instrutor} />
+			<Route exact path="/cadastro/aluno" component={Cadastro_aluno} />
+		</div>
+	</Router>
+);
 
 ReactDOM.render(routing, document.getElementById('root'));
 
