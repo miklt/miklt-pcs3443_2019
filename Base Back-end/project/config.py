71b1b5365af7a17b58ba91cf0c43dbdfe8b731da
davidengelstein <davidengelstein@hotmail.com>
2019-05-20 17:35:50 -0300

    Trocando URL para nuvem
    Usando o elephantsql

--- Base Back-end/project/config.py
+++ Base Back-end/project/config.py
@@ -3,4 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:78995115989@localhost/Escola_Aviacao"
+SQLALCHEMY_DATABASE_URI = "postgres://xeoaxapy:Hth8zCUpDje0moUNoglu689LeyB5ZF2g@tuffi.db.elephantsql.com:5432/xeoaxapy"
