97bde3c13d1e2fdd016de58727efd9ed8a318bb3
Thabata <tathabata.pontes@hotmail.com>
2019-05-20 21:54:34 -0300

    navbar atualizado

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,8 +13,10 @@ class MyNavbar extends Component {
           <Nav.Link href="#link">Link</Nav.Link>
           <NavDropdown title="Cadastro" id="basic-nav-dropdown">
             <NavDropdown.Item href="/cadastro/instrutor">Instrutor</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Voo Supervisionado</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Piloto</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Aluno</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Voo</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Voo Supervisionado</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
           </NavDropdown>
