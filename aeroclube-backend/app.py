876a0352594a5bc56387c649a6f28b5fc2d816ff
Joao Lins <jpelins@gmail.com>
2019-05-21 16:47:17 -0300

    "melhorada na template base"

--- aeroclube-backend/app.py
+++ aeroclube-backend/app.py
@@ -3,12 +3,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("base_admin.html")
 
 
 @app.route("/teste")
 def teste():
-    return render_template("teste.html")
+    return render_template("inicial_usuario.html")
+
+@app.route("/consulta-horas")
+def consultaHoras():
+    return render_template("inicial_usuario.html")
+
 
 
 def create_tables():
