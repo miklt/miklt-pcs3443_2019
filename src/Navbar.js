08ca212755713a82ddbb56f542036929e0611a08
Thabata <tathabata.pontes@hotmail.com>
2019-05-20 21:51:18 -0300

    navbar atualizado

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,6 +14,7 @@ class MyNavbar extends Component {
           <NavDropdown title="Cadastro" id="basic-nav-dropdown">
             <NavDropdown.Item href="/cadastro/instrutor">Instrutor</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">Piloto</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Aluno</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.3">Voo Supervisionado</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
