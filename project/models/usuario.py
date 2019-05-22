58530572b33f4aee015ac1fff00d972fa2261e72
Eric Imai <ericimai@hotmail.com>
2019-05-22 09:49:49 -0300

    Route usuario.py concluido

--- project/models/usuario.py
+++ project/models/usuario.py
@@ -1,10 +1,8 @@
-from datetime import datetime, time
 from project import db
-
 
 class Usuario(db.Model):
     __abstract__ = True
-    usuario_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(60), nullable=False, unique = True)
     cpf = db.Column(db.String(11), nullable=False, unique = True)
     email = db.Column(db.String(60), unique=True, nullable=False)
