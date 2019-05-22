3c484c4dec9eca067b940d764c9b9f8f7cc2d2f8
Thabata <tathabata.pontes@hotmail.com>
2019-05-22 09:10:30 -0300

    rebase cadastro-piloto to cadastro-instrutor

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Cadastro_voo from './cadastro/Voo';
 import Cadastro_piloto from './cadastro/Piloto';
 import Cadastro_voo_supervisionado from './cadastro/Voo_supervisionado';
 import Cadastro_aluno from './cadastro/Aluno';
-import Cadastro_voo_supervisionado from './cadastro/Voo_supervisionado';
 import * as serviceWorker from './serviceWorker';
 import Home from './Home';
 
@@ -20,7 +19,6 @@ const routing = (
 			<Route exact path="/cadastro/piloto" component={Cadastro_piloto} />
 			<Route exact path="/cadastro/voo_supervisionado" component={Cadastro_voo_supervisionado} />
 			<Route exact path="/cadastro/aluno" component={Cadastro_aluno} />
-      <Route exact path="/cadastro/voo_supervisionado" component={Cadastro_voo_supervisionado}/>
 		</div>
 	</Router>
 );
