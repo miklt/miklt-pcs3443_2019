0e33da15823bac29d30c4e9def9ae39223aea104
Gusgreco <gustavo.greco.85@gmail.com>
2019-05-22 13:15:14 -0300

    Inits e Métodos de busca e listar das classes

--- aeroclube-backend/aeroclube/models/aula_model.py
+++ aeroclube-backend/aeroclube/models/aula_model.py
@@ -9,3 +9,17 @@ class Aula(Voo):
         'aluno.id'), primary_key=True)
     id_instrutor = db.Column(db.Integer, db.ForeignKey(
         'instrutor.id'), primary_key=True)
+
+    def __init__(self, id_aluno, id_instrutor, duracao, data):
+        self.id_aluno = id_aluno
+        self.id_instrutor = id_instrutor
+        self.duracao = duracao
+        self.data = data
+
+    @classmethod
+    def encontrar_pelo_id_aluno(cls, id_aluno):
+        return cls.query.filter_by(id_aluno=id_aluno).first()
+
+    @classmethod
+    def encontrar_pelo_id_instrutor(cls, id_instrutor):
+        return cls.query.filter_by(id_instrutor=id_instrutor).first()
--- aeroclube-backend/aeroclube/models/certificado_model.py
+++ aeroclube-backend/aeroclube/models/certificado_model.py
@@ -1,4 +1,5 @@
 from dao import db, Base
+from datetime import datetime
 
 
 class Certificado(Base):
@@ -7,3 +8,27 @@ class Certificado(Base):
     id_aluno = db.Column(db.Integer, db.ForeignKey(
         'aluno.id'), primary_key=True)
     data = db.Column(db.DateTime)
+
+    def __init__(self, id_aluno):
+        self.id_aluno = id_aluno
+        self.data = datetime.now()
+
+    def adicionar(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def encontrar_pelo_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    @classmethod
+    def encontrar_pelo_id_aluno(cls, id_aluno):
+        return cls.query.filter_by(id_aluno=id_aluno).first()
+
+    @classmethod
+    def listar(cls):
+        return cls.query.all()
+
+    def remover(self):
+        db.session.delete(self)
+        db.session.commit()
