56755e8ef238529f015185a0c3de314f8376c1a1
Thabata <tathabata.pontes@hotmail.com>
2019-05-22 09:19:20 -0300

    rebase cadastro-voo to cadastro-instrutor

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import Cadastro_instrutor from './cadastro/Instrutor';
 import Cadastro_voo from './cadastro/Voo';
+import Cadastro_piloto from './cadastro/Piloto';
 import Cadastro_voo_supervisionado from './cadastro/Voo_supervisionado';
 import Cadastro_aluno from './cadastro/Aluno';
 import * as serviceWorker from './serviceWorker';
@@ -15,6 +16,7 @@ const routing = (
 			<Route exact path="/" component={Home} />
 			<Route exact path="/cadastro/instrutor" component={Cadastro_instrutor} />
 			<Route exact path="/cadastro/voo" component={Cadastro_voo} />
+			<Route exact path="/cadastro/piloto" component={Cadastro_piloto} />
 			<Route exact path="/cadastro/voo_supervisionado" component={Cadastro_voo_supervisionado} />
 			<Route exact path="/cadastro/aluno" component={Cadastro_aluno} />
 		</div>
