5dabaf9b6278e6b54a7e7b5e346a0c1f9ed7bb26
victorpassos1@hotmail.com <victorpassos1@hotmail.com>
2019-05-22 04:36:08 +0000

    Updated models.py

--- app/voo/models.py
+++ app/voo/models.py
@@ -1,21 +1,42 @@
 from app import db
 
 class Voo(db.Model):
-    __tablename__ = 'voo'
+    __abstract__ = True
 
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default = db.func.current_timestamp())
+    date_created = db.Column(db.DateTime, default = db.func.current_timestamp(),primary_key = True)
     date_modified = db.Column(db.DateTime, default = db.func.current_timestamp(),
                                            onupdate = db.func.current_timestamp())
 
     horaSaida = db.Column(db.String(128), nullable = False)
     duracao = db.Column(db.String(128), nullable = False, unique = True)
-    parecer = db.Column(db.String(192), nullable = False)
 
-    def __init__(self, horaSaida, duracao, parecer):
+    def __init__(self, horaSaida, duracao):
         self.horaSaida = horaSaida
         self.duracao = duracao
-        self.parecer = parecer
+
 
     def __repr__(self):
-        return '<Parecer %r>' % (self.parecer)
+        return '<Parecer %r>' % (self.)
+
+class Aula(Voo):
+    __tablename__ = "aula"
+    __mapper_args__ = {'polymorphic_identity': 'aula'}
+
+    
+    aluno = db.Column(db.String(128), ForeignKey('aluno.name'), primary_key = True)
+    instrutor = db.Column(db.String(128), ForeignKey('instrutor.name'))
+
+    parecer = db.Column(db.Integer, nullable = False)
+
+    def __init__(self,horaSaida,duracao,parecer):
+        super().__init__(horaSaida,duracao)
+        self.parecer = parecer
+
+class VooSimples(Voo):
+    __tablename__ = "voosimples"
+    __mapper_args__ = {'polymorphic_identity': 'voosimples'}
+
+    piloto = db.Column(db.String(128), ForeignKey('piloto.name'), primary_key = True)
+
+    def __init__(self,horaSaida,duracao):
+        super().__init__(horaSaida,duracao)
