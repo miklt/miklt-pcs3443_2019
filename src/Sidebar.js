4d8b71df5b0cee9908a7a70c8189b724bbd9dbed
Pedro Ligabue <pedro.ligabue@outlook.com>
2019-05-22 20:03:48 -0300

    fad

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -8,11 +8,11 @@ class Sidebar extends React.Component {
             <div>
                 <ul class="sidebarAba">
                     <li className="sidebarHeader">MENU</li>
-                    <li><Aba name="Nome1" nameHtml="./Nome1.html"/></li>
-                    <li><Aba name="Nome2" nameHtml="./Nome2.html"/></li>
-                    <li><Aba name="Nome3" nameHtml="./Nome3.html"/></li>
-                    <li><Aba name="Nome4" nameHtml="./Nome4.html"/></li>
-                    <li><Aba name="Nome5" nameHtml="./Nome5.html"/></li>
+                    <li><Aba name="Login" nameHtml="./Nome1.html"/></li>
+                    <li><Aba name="Agendamentos" nameHtml="./Nome2.html"/></li>
+                    <li><Aba name="Perfil" nameHtml="./Nome3.html"/></li>
+                    <li><Aba name="Informações" nameHtml="./Nome4.html"/></li>
+                    <li><Aba name="Configurações" nameHtml="./Nome5.html"/></li>
                 </ul>
                 <a href="clientes.html" class='botao'>Ver todos &raquo;</a>
             </div>
