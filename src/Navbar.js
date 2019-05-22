ed05d54157d3ff5ce94e22741985ab35e1002a84
Thabata <tathabata.pontes@hotmail.com>
2019-05-22 09:47:24 -0300

    update navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,25 +5,22 @@ class MyNavbar extends Component {
   render() {
     return (
       <Navbar bg="dark" expand="md" variant="dark" sticky="top">
-        <Navbar.Brand href="#home">Stop Down</Navbar.Brand>
+        <Navbar.Brand href="/">Stop Down</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
           <NavDropdown title="Cadastro" id="basic-nav-dropdown">
             <NavDropdown.Item href="/cadastro/instrutor">Instrutor</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Piloto</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Aluno</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Voo</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Voo Supervisionado</NavDropdown.Item>
+            <NavDropdown.Item href="/cadastro/piloto">Piloto</NavDropdown.Item>
+            <NavDropdown.Item href="/cadastro/aluno">Aluno</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            <NavDropdown.Item href="/cadastro/voo">Voo</NavDropdown.Item>
+            <NavDropdown.Item href="/cadastro/voo_supervisionado">Voo Supervisionado</NavDropdown.Item>
           </NavDropdown>
           </Nav>
           <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
+          <FormControl type="text" placeholder="Buscar" className="mr-sm-2" />
+          <Button variant="outline-success">Buscar</Button>
           </Form>
         </Navbar.Collapse>
       </Navbar>
