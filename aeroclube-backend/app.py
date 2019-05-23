7167884d70e726c76fd5cb93243d33a0c6570e62
Joao Lins <jpelins@gmail.com>
2019-05-22 22:05:07 -0300

    templates front end

--- aeroclube-backend/app.py
+++ aeroclube-backend/app.py
@@ -3,16 +3,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("base_admin.html")
+    return render_template("home.html")
 
+### USUARIO
+@app.route("/cadastrar_usuario")
+def cadastrarUsuario():
+    return render_template("cadastrar_usuario.html")
 
-@app.route("/teste")
-def teste():
-    return render_template("inicial_usuario.html")
+@app.route("/listar_usuario")
+def listarUsuario():
+    return render_template("listar_usuario.html")
 
-@app.route("/consulta-horas")
-def consultaHoras():
-    return render_template("inicial_usuario.html")
+### VOO
+@app.route("/cadastrar_voo")
+def cadastrarVoo():
+    return render_template("cadastrar_voo.html")
+
+@app.route("/listar_voo")
+def listarVoo():
+    return render_template("listar_usuario.html")
+
+### CONSULTA HORAS DE VOO
+@app.route("/consultar_horas")  
+def consultarHoras():
+    return render_template("consultar_horas.html")
+
+@app.route("/visualizar_horas")  
+def visualizarHoras():
+    return render_template("visualizar_horas.html")
 
 
 
