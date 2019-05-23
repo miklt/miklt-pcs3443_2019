3590bb305fa8dcddc170055763f0a7ae11b8ddc2
Aluno <aluno@labprog.local>
2019-05-23 10:11:04 -0300

    fasdf

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,43 @@
 import React from "react"
 import "./Login.css"
-
+import {url_v3} from "./App"
 
 class Login extends React.Component {
     constructor() {
         super();
-        this.onLogin = this.onLogin.bind(this)
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            user: '',
+            password: '',
+        }
     }
 
-    onLogin(event) {
-        event.preventDefault()
-        const data = new FormData(event.target)
-
-        fetch(url_v3+"/login", {
-            method: 'POST',
-            body: data,
-        })
+    handleChange(event) {
+        this.setState({[event.target.name] : event.target.value})
     }
+
+    handleSubmit(event){ 
+        event.preventDefault();
+        fetch(url_v3 + 'login', {
+            method: 'post',
+            body: {
+                user: this.state.user,
+                password: this.state.password,
+            }
+        });
+    };
 
     render () {
+        
+        console.log(this.state)
         return (
             <div>
-                <form onSubmit={this.onLogin} className="telaLogin">
+                <form className="telaLogin" onSubmit={this.handleSubmit}>
                     <label for="user">Usuário:</label>
-                    <input type="text" name="user" id="user"></input>
+                    <input type="text" name="user" id="user" onChange={this.handleChange}/>
                     <label for = "password">Senha:</label>
-                    <input type="password" name="password" id="password"></input>
+                    <input type="password" name="password" id="password"  onChange={this.handleChange}/>
                     <button type="submit">Go</button>
                 </form>
             </div>
