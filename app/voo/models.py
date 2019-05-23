55a9e7297e6f9a19e3ebf41825bd508de47a5915
Vitor Tiveron <vitortiveron@hotmail.com>
2019-05-23 14:11:20 +0000

    Updated models.py

--- app/voo/models.py
+++ app/voo/models.py
@@ -1,14 +1,15 @@
 from app import db
-
+from app import users
 class Voo(db.Model):
     __tablename__ = "voo"
     __mapper_args__ = {'polymorphic_identity': 'voo'}
 
     dataVoo = db.Column(db.DateTime, default = db.func.current_timestamp(),primary_key = True)
-   
-    aeronave = db.Column(db.Integer, ForeignKey('aeronave.id'),primary_key=True)
+    
+    #aeronave = db.Column(db.Integer, db.ForeignKey('aeronave.id'),primary_key = True)
 
     horaSaida = db.Column(db.String(128), nullable = False)
+
     duracao = db.Column(db.String(128), nullable = False, unique = True)
     
 
@@ -21,11 +22,13 @@ class Aula(Voo):
     __tablename__ = "aula"
     __mapper_args__ = {'polymorphic_identity': 'aula'}
 
-    
-    aluno = db.Column(db.String(128), ForeignKey('aluno.name'))
-    instrutor = db.Column(db.String(128), ForeignKey('instrutor.name'))
+    aluno = db.Column(db.String(128), db.ForeignKey('login.name'), primary_key = True)
+
+    instrutor = db.Column(db.String(128), db.ForeignKey('login.name'))
 
     parecer = db.Column(db.Integer, nullable = False)
+
+    dataAula = db.Column(db.DateTime,db.ForeignKey('voo.dataVoo'))
 
     def __init__(self,horaSaida,duracao,parecer):
         super().__init__(horaSaida,duracao)
@@ -35,13 +38,14 @@ class VooSimples(Voo):
     __tablename__ = "voosimples"
     __mapper_args__ = {'polymorphic_identity': 'voosimples'}
 
-    piloto = db.Column(db.String(128), ForeignKey('piloto.name'))
+    piloto = db.Column(db.String(128), db.ForeignKey('login.name'), primary_key = True)
+
+    dataVooSimples = db.Column(db.DateTime,db.ForeignKey('voo.dataVoo'))
 
     def __init__(self,horaSaida,duracao):
         super().__init__(horaSaida,duracao)
 
-
-
+        
 class Aeronave(db.Model):
     __tablename__ = 'aeronave'
     __mapper_args__ = {'polymorphic_identity': 'aeronave'}
