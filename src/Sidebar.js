ec6ba8d192341a5794bfdd46ea0d0e248ee5f323
Pedro Ligabue <pedro.ligabue@outlook.com>
2019-05-24 12:05:26 -0300

    asdf

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -64,6 +64,7 @@ class Sidebar extends React.Component {
                         <ul className="sidebarAba">
                             <li className="sidebarHeader">MENU</li>
                             <li><Aba name="Agendamentos" nameHtml="./Agendamento.html"/></li>
+                            <li><Aba name="Sócios" nameHtml="./Sócios.html"/></li>
                             <li><Aba name="Perfil" nameHtml="./Perfil.html"/></li>
                             <li><Aba name="Configurações" nameHtml="./Configuracoes.html"/></li>
                             <li><Aba name="Log Out" nameHtml="./Logout.html"/></li>
