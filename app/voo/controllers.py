01152559954029997245593d55943c47caf2bee8
Vitor Tiveron <vitortiveron@hotmail.com>
2019-06-06 15:00:37 +0000

    Updated controllers.py

--- app/voo/controllers.py
+++ app/voo/controllers.py
@@ -3,6 +3,7 @@ from app import db
 from flask import request
 import app.voo.models as models
 from sqlalchemy.sql import func,select
+import json
 
 from datetime import datetime
 
@@ -13,37 +14,36 @@ voo = Blueprint('voo', __name__)
 def agendarVoo():
     data=request.get_json()
     tipo=data['tipo']   
-    print(tipo)
+
     if tipo == 'Voo Simples':
         v=models.VooSimples(
             piloto=data['piloto'],
-            dataVoo=datetime.strptime(data['dataVoo'], '%Y-%m-%d'),
+            dataVoo=db.func.current_timestamp(),
             horaSaida=data['horaSaida'],
             duracao=data['duracao'],
             aeronave=data['aeronave'],
-            tipo=data['tipo']
-  )
+            tipo=data['tipo'],
+            matricula=data['matricula']
+        )
     
     elif tipo == 'Aula':
         v=models.Aula(
             aluno=data['aluno'],
             instrutor=data['instrutor'],
             parecer='0',
-            dataVoo=datetime.strptime(data['dataVoo'],'%Y-%m-%d'),
+            dataVoo=db.func.current_timestamp(),
             horaSaida=data['horaSaida'],
             duracao=data['duracao'],
             aeronave=data['aeronave'],
-            tipo=data['tipo']
+            tipo=data['tipo'],
+            matricula=data['matricula']
            
-        )
+       )
+
+    else: 
+       return "PeePooPoo"
     
             
-
-    #if data['tipo']=='Voo simples':
-        #v=models.VooSimples(data['piloto'],data['dataVooSimples'],data['horaSaida'],data['duracao'],data['aeronave'], data['tipo'])
-    #if data['tipo']=='Aula':
-        #v=models.Aula(data['aluno'],data['instrutor'],data['parecer'],data['dataVoo'],data['horaSaida'],data['duracao'],data['aeronave'],data['tipo'])
- 
     #v=models.VooSimples("Maria",db.func.current_timestamp(),"112","111","1","Voo Simples")
     #g=models.Aula("Joao","Maria","5",db.func.current_timestamp(),"11","2","4","Aula")
     
@@ -53,14 +53,23 @@ def agendarVoo():
 
     return "great success"
 
-@voo.route('/consultarAula/', methods=["GET"])
-def consultarAula():
-    #data=request.get_json()
-    data="oi"
-    sample=models.Aula.query.filter_by(aluno ="Joao").all()
-    print(sample)
-    return "oi"
+@voo.route('/consultaAula', methods=["POST"])
+def consultaAula():
+       data=request.get_json()
+       sample=models.Aula.query.filter_by(matricula=data['matricula']).first()
+       v={'matricula' : sample.matricula,
+          'aluno'     : sample.aluno,
+          'instrutor' : sample.instrutor,
+          'parecer'   : sample.parecer, 
+          #'dataVoo'   : sample.dataVoo,
+          #'horaSaida' : sample.horaSaida,
+          #'duracao'   : sample.duracao,
+          #'aeronave'  : sample.aeronave 
+        }
+    
+       return json.dumps(v)
 
+ 
 @voo.route('/consultarHoras/',methods=["GET"])
 def consultarHoras():
     #data=request.get_json()
