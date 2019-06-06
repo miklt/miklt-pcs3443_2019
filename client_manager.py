5b7f0da6860c79ac22df416ac8a9b3a5ee318fb6
gabivsguedes <48091006+gabivsguedes@users.noreply.github.com>
2019-06-06 14:39:19 -0300

    Add files via upload
    versao final - falta verificacao completa

--- client_manager.py
+++ client_manager.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,url_for
 import os 
 from flask_sqlalchemy import SQLAlchemy
+import datetime,random
 #from db_setup import init_db, db_session
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
@@ -11,7 +12,8 @@ database_file = "sqlite:///{}".format(os.path.join(project_dir,'client_database.
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SQLALCHEMY_BINDS"] = {
-	'voos': "sqlite:///{}".format(os.path.join(project_dir,'voos.db'))
+	'voos': "sqlite:///{}".format(os.path.join(project_dir,'voos.db')),
+	'login': "sqlite:///{}".format(os.path.join(project_dir,'login.db'))
 }
 
 db = SQLAlchemy(app)
@@ -34,7 +36,11 @@ class Socio(db.Model):
 	numero_brevet= db.Column(db.String(80), nullable = True)
 
 	def __repr__(self):
-		return "<Nome: {} e matricula {}>".format(self.nome)#,self.matricula)
+		return "<Nome: {} e tipo {}>".format(self.nome,self.tipo)#,self.matricula)
+
+class Login(db.Model):
+	__bind_key__ = 'login'
+	matricula = db.Column(db.Integer(),unique = True, nullable = False,primary_key=True)
 
 class Voos(db.Model):
 	__bind_key__ = 'voos'
@@ -50,8 +56,9 @@ class Voos(db.Model):
 def home():
 	if request.form:
 		print(request.form)
+		matricula = Socio.query.count()
 		aluno = Socio(nome = request.form.get('nome'),
-						matricula = request.form.get('matricula'),
+						matricula = matricula + 1,
 						senha = request.form.get('senha'),
 						sobrenome = request.form.get('sobrenome'),
 						idade = request.form.get('idade'),
@@ -72,12 +79,40 @@ def home():
 		#alunos = Aluno.query.all()
 	return render_template('TelaCadastroAluno.html')
 
+@app.route('/cadastro-funcionario',methods =["GET","POST"])
+def homee():
+	if request.form:
+		print(request.form)
+		matricula = Socio.query.count()
+		aluno = Socio(nome = request.form.get('nome'),
+						matricula = matricula + 1,
+						senha = request.form.get('senha'),
+						sobrenome = request.form.get('sobrenome'),
+						idade = request.form.get('idade'),
+						telefone = request.form.get('telefone'),
+						endereco = request.form.get('endereco'),
+						email = request.form.get('email'),
+						cpf = request.form.get('cpf'),
+						sexo = request.form.get('sexo'),
+						numero_brevet = None,
+						instituicao = None,
+						tipo = 'funcionario',
+						data_diploma = None,
+						numero_horas = 0,
+
+						)
+		db.session.add(aluno)
+		db.session.commit()
+		#alunos = Aluno.query.all()
+	return render_template('TelaCadastroFuncionario.html')
+
 @app.route('/cadastro-piloto',methods =["GET","POST"])
 def home1():
 	if request.form:
 		print(request.form)
+		matricula = Socio.query.count()
 		piloto = Socio(nome = request.form.get('nome'),
-						matricula = request.form.get('matricula'),
+						matricula = matricula + 1,
 						senha = request.form.get('senha'),
 						sobrenome = request.form.get('sobrenome'),
 						idade = request.form.get('idade'),
@@ -101,16 +136,18 @@ def visualizar():
 	alunos = Socio.query.filter_by(tipo = 'aluno')
 	pilotos = Socio.query.filter_by(tipo = 'piloto')
 	instrutores = Socio.query.filter_by(tipo = 'instrutor')
+	funcionarios = Socio.query.filter_by(tipo = 'funcionario')
 	db.session.commit()
 	return render_template('Visualizar.html',alunos = alunos
-						,pilotos = pilotos,instrutores = instrutores)
+						,pilotos = pilotos,instrutores = instrutores,funcionarios = funcionarios)
 
 @app.route('/cadastro-instrutor',methods =["GET","POST"])
 def home2():
 	if request.form:
 		print(request.form)
+		matricula = Socio.query.count()
 		instrutor = Socio(nome = request.form.get('nome'),
-						matricula = request.form.get('matricula'),
+						matricula = matricula + 1,
 						senha = request.form.get('senha'),
 						sobrenome = request.form.get('sobrenome'),
 						idade = request.form.get('idade'),
@@ -134,7 +171,11 @@ def home2():
 def home3():
 	return render_template('ConsultaSocio.html')
 
-@app.route('/inicial',methods =["GET","POST"])
+@app.route('/consultar-instrutor',methods =["GET","POST"])
+def homee3():
+	return render_template('ConsultaSocioInstrutor.html')
+
+@app.route('/inicial',methods =["GET","POST"]) # tela inicial funcionario
 def home4():
 	return render_template('TelaInicial.html')
 
@@ -146,30 +187,78 @@ def home6():
 		socios = Socio.query.filter_by(matricula = matricula)
 	return render_template('ResultadoConsultaSocio.html',socios = socios)
 
+@app.route('/resultado-consulta-instrutor',methods =["GET","POST"])
+def hommee6():
+
+	if request.method =='POST':
+		matricula = request.form.get('matricula')
+		socios = Socio.query.filter_by(matricula = matricula)
+	return render_template('ResultadoConsultaSocioInstrutor.html',socios = socios)
+
+@app.route('/visualiza-cadastro-individual',methods =["GET","POST"])
+def resultado_consulta_aluno():
+
+	matricula = Login.query.first().matricula
+	socios = Socio.query.filter_by(matricula = matricula)
+	socio = socios.first()
+	if(socio.tipo == 'aluno'):
+		if(int(socio.numero_horas)>= 35):
+			print(socio)
+			socio.tipo = 'piloto'
+			socio.instituicao = 'Poli'
+			socio.data_diploma = datetime.datetime.now()
+			socio.numero_brevet = random.randint(1,1000000)
+			print(socio)
+			db.session.commit()
+			return redirect(url_for('emissao_brevet',nome = socio.nome,horas = socio.numero_horas,matricula = matricula))
+	return render_template('ResultadoConsultaSocioIndividual.html',socios = socios)
+
 @app.route('/cadastro-voo',methods =["GET","POST"])
 def home7():
 	if request.form:
+		matricula = Login.query.first().matricula
+		socios = Socio.query.filter_by(matricula = matricula)
 		print(request.form)
-		voo = Voos(numero_voo = request.form.get('numero_voo'),
+		mat_aluno = request.form.get('matricula_aluno')
+		horas = request.form.get('horas_de_voo')
+		numero = Voos.query.count()
+		voo = Voos(numero_voo = numero+ 1,
 					aluno = request.form.get('matricula_aluno'),
-					instrutor = request.form.get('matricula_instrutor'),
+					instrutor = matricula,
 					horas = request.form.get('horas_de_voo'),
 					rate = request.form.get('rate'),
 					data_hora = request.form.get('data_hora_voo'),
 			)
 		db.session.add(voo)
+		aluno = Socio.query.filter_by(matricula = mat_aluno).first()
+		aluno.numero_horas = int(aluno.numero_horas) + int(horas)
 		db.session.commit()
+
 	return render_template('TelaCadastroVoo.html')
 
 @app.route('/',methods =["GET","POST"])
 def home8():
 	if request.method == "POST":
+		Login.query.delete()
 		matricula = request.form.get('matricula')
 		senha = request.form.get('senha')
+		login = Login(matricula = matricula)
+		db.session.add(login)
+		db.session.commit()
 		if not(senha == '' or matricula == ''):
 			socio = Socio.query.filter_by(matricula = matricula).first()
 			if (senha== socio.senha):
-				return redirect('/inicial')
+				print(socio)
+				if(socio.tipo == 'aluno'):
+					return redirect(url_for('inicial_aluno'))	
+				elif(socio.tipo == 'instrutor'):
+					return redirect(url_for('inicial_instrutor'))
+				elif(socio.tipo == 'piloto'):
+					return redirect(url_for('inicial_aluno'))
+				elif(socio.tipo == 'funcionario'):
+					return redirect(url_for('inicial_funcionario'))
+				else:
+					return redirect('/inicial')
 			else:
 				erro = 'Senha incorreta'
 
@@ -184,12 +273,38 @@ def home10():
 	if request.method =='POST':
 		numero_voo = request.form.get('numero_voo')
 		voos = Voos.query.filter_by(numero_voo = numero_voo)
-	return render_template('ResultadoVoo.html',voos = voos)
+	return render_template('ResultadoConsultaVoo.html',voos = voos)
+
+
+@app.route('/consultar-voos',methods =["GET","POST"])
+def homme1():
+	matricula = Login.query.first().matricula
+	voos = Voos.query.filter_by(aluno = matricula)
+	return render_template('ResultadoConsultaVooIndividual.html',voos = voos)
 
 @app.route('/listar-voo',methods =["GET","POST"])
 def home11():
 	voos = Voos.query.all()
-	return render_template('ListarVoo.html',voos = voos)
+	return render_template('ResultadoConsultaVoo.html',voos = voos)
+
+@app.route('/emissao-brevet',methods =["GET","POST"])
+def emissao_brevet():
+	nome = request.args.get('nome')
+	matricula = request.args.get('matricula')
+	horas = request.args.get('horas')
+	return render_template('EmitirBreve.html',nome = nome,matricula= matricula,horas= horas)
+
+@app.route('/inicial-individual',methods =["GET","POST"])
+def inicial_aluno():
+	return render_template('TelaInicialAluno.html')
+
+@app.route('/inicial',methods =["GET","POST"])
+def inicial_funcionario():
+	return render_template('TelaInicial.html')
+
+@app.route('/inicial-instrutor',methods =["GET","POST"])
+def inicial_instrutor():
+	return render_template('TelaInicialInstrutor.html')
 
 
 if __name__ == '__main__':
