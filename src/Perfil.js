bd9f106c7afefb2812ea603292c0696f1c94298a
Aluno <aluno@labprog.local>
2019-06-06 10:44:11 -0300

    ihyhgfkjl

--- src/Perfil.js
+++ src/Perfil.js
@@ -2,34 +2,48 @@ import React from "react"
 import "./App.css"
 import axios from "axios"
 import {url_v3} from "./App"
+import { getToken } from "./Auth";
 
 class Perfil extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: "",
-            role: "",
-            horas: "",
-        }
-    }
 
-    getState(event) {
-        const url = url_v3+'perfil'+this.props.state.user;
-        axios.get(url)
+    getState() {
+        
+        const matricula = getToken();
+
+        const url = url_v3+"perfil/init";
+        axios.post(url,{
+            matricula: matricula
+        })
             .then(response => {
                 console.log(response)
-            })
-            .catch(error => {
-                console.log(error)
+            }).catch(error => {
+                console.log("deu ruim")
             });
+        console.log(matricula);
+
     }
+    
+    constructor(props) {
+        super(props);
+        this.state = {}
+        this.getState = this.getState.bind(this);
+        console.log(this.props.state);
+        this.getState();
+    }
+
     render () {
 
         return (
-            <div>
-                <h1>{this.state.role}: {this.state.name}</h1>
-                <h2>Banco de horas: {this.state.horas}</h2>
+            <div classname = "perfil">
+                <h1>{this.props.state.role}: {this.props.state.name}</h1>
+                
+                <h2>Banco de horas: Total de horas{this.state.horas}</h2>
+
+                <ul className = "agendamentos">
+                    <li></li>
+                </ul>
+
             </div>
         )
     }
