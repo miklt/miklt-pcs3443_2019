8751eded27d49e2c31759d3bc7de3976a3e31341
Gabriel <gmduarte96@gmail.com>
2019-06-06 11:28:57 -0300

    Front-end aluno check

--- src/app/models/voo.model.ts
+++ src/app/models/voo.model.ts
@@ -6,6 +6,16 @@ export class Voo {
   parecer: number;
   aluno: number;
   instrutor: number;
+  constructor(obj?: object) {
+    Object.assign(this, obj);
+  }
+  getTempoDeVoo() {
+    const saida = new Date();
+    const chegada = new Date();
+    chegada.setHours(Number(this.horarioChegada.slice(0, 2)), Number(this.horarioChegada.slice(2, 4)));
+    saida.setHours(Number(this.horarioSaida.slice(0, 2)), Number(this.horarioSaida.slice(2, 4)));
+    return ((Number(chegada) - Number(saida)) / (1000 * 60)).toFixed(0);
+  }
 }
 
 export interface APIResponse<T> {
