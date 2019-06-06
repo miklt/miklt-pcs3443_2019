cd5f68b51fb42833409a0483738d686a4bd063ec
Aluno <aluno@labprog.local>
2019-06-06 11:08:27 -0300

    avatar

--- src/Socios.js
+++ src/Socios.js
@@ -1,10 +1,31 @@
 import React from "react"
+import axios from 'axios'
+import { REPLServer } from "repl";
 
 class Socios extends React.Component {
+    constructor(props) {
+        super(props)
+        MontarTabela()
+    }
+
+    MontarTabela() {
+        var table = document.getElementById("myTable")
+        axios.get(url) 
+            .then(response => {
+                console.log(response)
+                /*var row = table.insertRow(0)
+                var cellId = row.insertCell(0)
+                var cellName = row.insertCell(1)
+                cellId.innerHTML = response.data.matricula
+                cellName.innerHTML = response.data.name*/
+            }
+        
+    }
+
     render () {
         return (
             <div>
-                 <h1>~LISTA DE SOCIOS~</h1>   
+                <table id="myTable"></table>  
             </div>
         )
     }
