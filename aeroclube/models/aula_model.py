15154c1f3a9cc2af1fb864583750910c0be0419d
Gusgreco <gustavo.greco.85@gmail.com>
2019-06-06 09:00:23 -0300

    banco voos

--- aeroclube/models/aula_model.py
+++ aeroclube/models/aula_model.py
@@ -1,16 +1,19 @@
-from dao import db
-from aeroclube.models.voo_model import Voo
+from dao import db, Base
 
 
-class Aula(Voo):
+class Aula(Base):
     __tablename__ = 'aula'
     id = db.Column(db.Integer, db.ForeignKey('voo.id'), primary_key=True)
     id_aluno = db.Column(db.Integer, db.ForeignKey(
         'pessoa.id'), primary_key=True)
     id_instrutor = db.Column(db.Integer, db.ForeignKey(
         'pessoa.id'), primary_key=True)
+    data = db.Column(db.DateTime)
+    duracao = db.Column(db.Integer)
+    avaliacao = db.Column(db.String(200), unique=True)
+    nota = db.Column(db.Integer)
 
-    def __init__(self, id_aluno, id_instrutor, duracao, data):
+    def __init__(self, id_aluno, id_instrutor, data, duracao):
         self.id_aluno = id_aluno
         self.id_instrutor = id_instrutor
         self.duracao = duracao
@@ -23,3 +26,7 @@ class Aula(Voo):
     @classmethod
     def encontrar_pelo_id_instrutor(cls, id_instrutor):
         return cls.query.filter_by(id_instrutor=id_instrutor).first()
+
+    @classmethod
+    def encontrar_pela_data(cls, data):
+        return cls.query.filter_by(data=data).first()
