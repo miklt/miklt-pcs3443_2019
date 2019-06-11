b1d40d954ff0cb8856dde9cd6d14eec0779fe4c5
henriquecv <h.c.21@hotmail.com>
2019-06-11 20:58:05 -0300

    ss

--- Base Back-end/engsoft_backend/config.py
+++ Base Back-end/engsoft_backend/config.py
@@ -3,5 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgres://xeoaxapy:Hth8zCUpDje0moUNoglu689LeyB5ZF2g@tuffi.db.elephantsql.com:5432/xeoaxapy"
-#SQLALCHEMY_DATABASE_URI = "postgres://postgres:1234@localhost/labengsoft"
+#SQLALCHEMY_DATABASE_URI = "postgres://xeoaxapy:Hth8zCUpDje0moUNoglu689LeyB5ZF2g@tuffi.db.elephantsql.com:5432/xeoaxapy"
+SQLALCHEMY_DATABASE_URI = "postgres://postgres:1234@localhost/labengsoft"
