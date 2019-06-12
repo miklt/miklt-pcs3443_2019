c57cb4d86df95b92ba29aac3db8c2890b52cbfae
Joao Lins <jpelins@gmail.com>
2019-06-12 17:46:48 -0300

    db 2

--- app.py
+++ app.py
@@ -832,4 +832,4 @@ def create_tables():
 if __name__ == '__main__':
     from dao import db
     db.init_app(app)
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
