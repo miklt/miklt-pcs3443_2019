c5ba34c2b10a96cb244ab5d0f9d991ed143214a2
Vitor Tiveron <vitortiveron@hotmail.com>
2019-06-13 13:00:40 +0000

    Updated ConsultaAula.js

--- src/ConsultaAula.js
+++ src/ConsultaAula.js
@@ -1,84 +1,129 @@
 import React from "react"
+import axios from 'axios'
 import "./ConsultaAula.css"
-import {url_v3} from "./App"
-import axios from 'axios';
-
+import { url_v3 } from "./App";
 
 class ConsultaAula extends React.Component {
-    
-    constructor() {
-        super();
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-
-        this.state = {
-            matricula: '',
-            isVisible: false,  /// NOVO ATRIBUTO            
-        };
-    }
-    handleChange(event) {
-        this.setState({[event.target.name] : event.target.value})
-    }
-
-    handleSubmit(event){
-        event.preventDefault();
-
-        const url = url_v3+'consultaAula';
-        console.log(this.state.matricula)
-        axios.post(url, {
-            matricula: this.state.matricula,
-        }).then(response => {
-            console.log("estou aqui")
-            console.log(response.data)
-            
-           var data = document.createElement ("div")
-           var text = document.createTextNode(response.data.aluno)
-          // text += " "
-           //text += document.createTextNode(response.data.instrutor)
-           data.appendChild(text)
-     
-           
-        })
-        .catch(error => {
-            alert(error)
-        });
-
-        this.setState({       /// PÕE UM setState NO FINAL DO handleSubmit
-            isVisible : true,
-        })
-    };
-
-    render () {
+    constructor(props) {
+        super(props)
+        this.montarTabela = this.montarTabela.bind(this)
+        this.limpaTabela = this.limpaTabela.bind(this)
+        this.handleSubmit=this.handleSubmit.bind(this)
         
-        return (
-            
-            <div>
-                <form className="telaConsultaAula" onSubmit={this.handleSubmit}>
+        this.limpaTabela()
+        this.montarTabela()           
+    }
 
-                    <label htmlFor="matricula">Matrícula do Aluno:</label>
-                    <input type="text" name="matricula" id="matricula" onChange={this.handleChange} required />
-                    <br />
+    limpaTabela() {
+        var Parent = document.getElementById("myTable");
+        if (Parent != null && Parent.hasChildNodes()) {
+            while (Parent.hasChildNodes()) {
+                Parent.removeChild(Parent.firstChild);
+            }
+        }
+    }
 
-                    <button type="submit" onSubmit={this.handleSubmit}>Go</button>
-                    {this.state.isVisible  &&    /// PÕE O isVisible AQUI
-                     <div>
-                        <label>Aluno:</label>
-                   
-                        <div id="aluno"></div>
-                        <br />
-                          
-                        <label>Instrutor:</label>
-                        <div id="instrutor"></div>
-                        <br />
-                    </div>
-                    }               
-                  
-                </form>
-            </div>
+    montarTabela() {
+        this.limpaTabela()
+        const url = url_v3+"consultaAula" 
+        axios.get(url)
+            .then(response => {
+                console.log(response.data)
+                var table = document.getElementById("myTable")
+                response.data.forEach(data => {
+                    
                     
 
+                    var row = table.insertRow(-1)
+
+                    var matricula = row.insertCell(-1)
+                    var aluno = row.insertCell(-1)
+                    var instrutor = row.insertCell(-1)
+                    var parecer = row.insertCell(-1)
+                    var dataAula = row.insertCell(-1)
+                    var numAula = row.insertCell(-1)
+                    
+                 
+                    var del = row.insertCell(-1)
+                    
+                    
+                    matricula.innerHTML = data.matricula
+                    aluno.innerHTML = data.aluno
+                    instrutor.innerHTML = data.instrutor
+                    parecer.innerHTML = data.parecer
+                    dataAula.innerHTML = data.dataAula
+                    numAula.innerHTML = data.numAula
+                    
+                    var button = document.createElement("button");
+                    button.innerHTML = "1";
+                    button.value = data.numAula+'/'+'1';
+                    button.onclick = this.handleSubmit;
+                    del.appendChild(button);
+                    console.log(button.value)
+                    var button = document.createElement("button");
+                    button.innerHTML = "2";
+                    button.value = data.numAula+'/'+'2';
+                    button.onclick = this.handleSubmit;
+                    del.appendChild(button);
+                    console.log(button.value)
+                    var button = document.createElement("button");
+                    button.innerHTML = "3";
+                    button.value = data.numAula+'/'+'3';
+                    button.onclick = this.handleSubmit;
+                    del.appendChild(button);
+                    console.log(button.value)
+                    var button = document.createElement("button");
+                    button.innerHTML = "4";
+                    button.value = data.numAula+'/'+'4';
+                    button.onclick = this.handleSubmit;
+                    del.appendChild(button);
+                    console.log(button.value)
+                    var button = document.createElement("button");
+                    button.innerHTML = "5";
+                    button.value = data.numAula+'/'+'5';
+                    button.onclick = this.handleSubmit;
+                    del.appendChild(button);
+                    console.log(button.value)
+                })
+            })
+        
+    }
+
+    handleSubmit(e) {
+    
+        const url = url_v3 + "consultaAula/" + e.target.value
+        
+        axios.put(url)
+        .then((response)=>{
+            console.log(response.data)
+               
+            
+        })
+        };
+    
+    render () {
+        return (
+            <div>
+                <table className="tabelaAula" style={{position : "relative", left : "50px", top : "16px"}}>
+                    <thead>
+                        <tr>
+                            <th>Matrícula</th>
+                            <th>Aluno</th>
+                            <th>Instrutor</th>
+                            <th>Parecer</th>
+                            <th>Data da Aula</th>
+                            <th>Duração</th>
+                            <th>Alterar Parecer</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody id="myTable">
+                        
+                    </tbody>
+                </table>  
+            </div>
         )
     }
-}
 
+}
 export default ConsultaAula
